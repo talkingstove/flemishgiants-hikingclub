8efc5483350f0e07ce4d785e8c900710226b6001
Ryan Moore <talkingstove@gmail.com>
2017-09-11 16:25:29 -0700

    stuff

--- static-asset-server/public/userDefinedComponents.js
+++ static-asset-server/public/userDefinedComponents.js
@@ -6,16 +6,18 @@ userDefinedComponents = {
           params = params || {};
           base.init(params);
 
-          console.log('%%%%%%% using usee defifned component!');
+          this.template = params.template || `
+            <ul data-data_binding="listItems" data-template_binding="compiledListItemTemplate">        
+            </ul>
+          `;
 
           this.listItemTemplate = params.listItemTemplate || `
             <li>
               <div>
                 <img class="hiker-bio-image" src="{{imgUrlFull}}" />
               </div>
-              
+
               {{firstName}} {{lastName}}
-              
 
               <p>
                 {{bio}}
@@ -23,14 +25,34 @@ userDefinedComponents = {
             </li>
           `;
 
-          // this.data = { 
-          //   listItems: null //expect []
-          // };
-          
           // //give it its own template not that of the superclass!!
-          // this.compiledTemplate = templateUtils.compileTemplate(this.template); //this.Handlebars.compile(this.template);
+          this.compiledTemplate = L.templateUtils.compileTemplate(this.template); //this.Handlebars.compile(this.template);
           this.compiledListItemTemplate = L.templateUtils.compileTemplate(this.listItemTemplate);
         }  
       }
-  })
+  }),
+  'hikeCalendar': L.componentDefinitions.L_List.extend(function(base) {
+        return {
+          // The `init` method serves as the constructor.
+          init: function(params) {
+            params = params || {};
+            base.init(params);
+
+            this.template = params.template || `
+              <ul data-data_binding="listItems" data-template_binding="compiledListItemTemplate">        
+              </ul>
+            `;
+
+            this.listItemTemplate = params.listItemTemplate || `
+              <li>
+                this is a hike soon go!
+              </li>
+            `;
+
+            // //give it its own template not that of the superclass!!
+            this.compiledTemplate = L.templateUtils.compileTemplate(this.template); //this.Handlebars.compile(this.template);
+            this.compiledListItemTemplate = L.templateUtils.compileTemplate(this.listItemTemplate);
+          }  
+        }
+    })
 }
